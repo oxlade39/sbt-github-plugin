6dd2d01a6f3b2d0fa5fc0c19febdb8621baf668e
Dan Oxlade <dan.oxlade@gmail.com>
2010-11-21 22:15:16 +0000

    more changes to the extractor tests

--- src/test/scala/com/github/oxlade39/githubuploader/GitHubRepositoryExtractorTest.scala
+++ src/test/scala/com/github/oxlade39/githubuploader/GitHubRepositoryExtractorTest.scala
@@ -16,14 +16,16 @@ object GitHubRepositoryExtractorSpec extends Specification {
     }
 
     "extract a GitHubRepository from the default HTTP URL" in {
-      GitHubRepositoryExtractor("https://oxlade39@github.com/oxlade39/scala.tmbundle.git") mustEqual Some(GitHubRepository("oxlade39", "scala.tmbundle"))
-      GitHubRepositoryExtractor("git://github.com/Constellation/ruby-net-github-upload.git") mustEqual Some(GitHubRepository("Constellation", "ruby-net-github-upload"))
       GitHubRepositoryExtractor("https://github.com/mirrors/linux-2.6.git") mustEqual Some(GitHubRepository("mirrors", "linux-2.6"))
     }
 
+    "extract a GitHubRepository from the private HTTP URL" in {
+      GitHubRepositoryExtractor("https://oxlade39@github.com/oxlade39/scala.tmbundle.git") mustEqual Some(GitHubRepository("oxlade39", "scala.tmbundle"))
+    }
+
     "extract a GitHubRepository from the readonly HTTP URL" in {
-      val httpURL = "git://github.com/oxlade39/spring-reference-project.git"
-      GitHubRepositoryExtractor(httpURL) mustEqual Some(GitHubRepository("oxlade39", "spring-reference-project"))
+      GitHubRepositoryExtractor("git://github.com/oxlade39/spring-reference-project.git") mustEqual Some(GitHubRepository("oxlade39", "spring-reference-project"))
+      GitHubRepositoryExtractor("git://github.com/Constellation/ruby-net-github-upload.git") mustEqual Some(GitHubRepository("Constellation", "ruby-net-github-upload"))
     }
   }
 }
